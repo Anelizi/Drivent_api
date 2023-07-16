a44b5b5b3105326c0474cdf25bd77f8c706a7713
Anelizi <anelizicarvalho@gmail.com>
2023-07-16 18:08:56 -0300

    fix: GET /hotels/:hotelId

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -1,31 +1,35 @@
-import { AuthenticatedRequest } from "@/middlewares";
+import { AuthenticatedRequest } from '@/middlewares';
 import { Response } from 'express';
-import httpStatus from "http-status";
+import httpStatus from 'http-status';
 import * as hotelServer from '@/services/hotel-service/index';
 
 export async function getHotel(req: AuthenticatedRequest, res: Response) {
-    const { userId } = req;
-    console.log(userId);
-    try {
-      const hotel = await hotelServer.getHotel(Number(userId));
+  const { userId } = req;
+  console.log(userId);
+  try {
+    const hotel = await hotelServer.getHotel(Number(userId));
 
-      return res.status(httpStatus.OK).send(hotel);
-    } catch (error) {
-        if(error.name === 'NotFoundError'){
-            return res.sendStatus(httpStatus.OK);
-        }
-      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    return res.status(httpStatus.OK).send(hotel);
+  } catch (error) {
+    if (error.name === 'NotFoundError') {
+      return res.sendStatus(httpStatus.OK);
     }
+    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+  }
 }
 
 export async function getHotelId(req: AuthenticatedRequest, res: Response) {
-    const { userId } = req;
-    const { hotelId } = req.params;
-    console.log(hotelId);
-    try {
-    //   const ticketTypes = await hotelServer.getTicketType();
-    //   return res.status(httpStatus.OK).send(ticketTypes);
-    } catch (e) {
-      return res.sendStatus(httpStatus.NO_CONTENT);
+  const { userId } = req;
+  const { hotelId } = req.params;
+  console.log(hotelId);
+  try {
+    const hotel = await hotelServer.getHotelId(Number(userId), Number(hotelId));
+
+      return res.status(httpStatus.OK).send(hotel);
+  } catch (error) {
+    if (error.name === 'NotFoundError') {
+      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
     }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
 }
--- src/services/hotel-service/index.ts
+++ src/services/hotel-service/index.ts
@@ -1,36 +1,36 @@
-import { notFoundError } from "@/errors";
-import enrollmentRepository from "@/repositories/enrollment-repository";
-import ticketsRepository from "@/repositories/tickets-repository";
-import * as hotelRepository from "@/repositories/hotel-repository/index";
+import { notFoundError } from '@/errors';
+import enrollmentRepository from '@/repositories/enrollment-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
+import * as hotelRepository from '@/repositories/hotel-repository/index';
 
 export async function hotelsList(userId: number) {
-    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
 
-    if(!enrollment) throw notFoundError();
+  if (!enrollment) throw notFoundError();
 
-    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
 
-    if(!ticket || ticket.status === 'RESERVED' || !ticket.TicketType.includesHotel){
-        throw notFoundError();
-    }
+  if (!ticket || ticket.status === 'RESERVED' || !ticket.TicketType.includesHotel || ticket.TicketType.isRemote) {
+    throw notFoundError();
+  }
 }
 
 export async function getHotel(userId: number) {
-    await hotelsList(userId);
+  await hotelsList(userId);
 
-    const hotel = await hotelRepository.findHotel();
+  const hotel = await hotelRepository.findHotel();
 
-    if(!hotel) throw notFoundError();
-    
-    return hotel;
+  if (!hotel) throw notFoundError();
+
+  return hotel;
 }
 
 export async function getHotelId(userId: number, hotelId: number) {
-    await hotelsList(userId);
+  await hotelsList(userId);
 
-    const hotel = await hotelRepository.findHotelById(hotelId);
+  const hotel = await hotelRepository.findHotelById(hotelId);
 
-    if(!hotel) throw notFoundError();
+  if (!hotel) throw notFoundError();
 
-    return hotel;
+  return hotel;
 }
