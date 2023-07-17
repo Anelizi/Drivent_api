6b5f8f82aeee38d0238202c19799aa2ef4ec807f
Anelizi <anelizicarvalho@gmail.com>
2023-07-16 23:04:53 -0300

    fix: tests

--- tests/integration/hotel.test.ts
+++ tests/integration/hotel.test.ts
@@ -20,14 +20,4 @@ describe('GET /hotels', () => {
 
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
-
-  describe('when token is valid', () => {
-    it('should respond with status 404 when user doesnt have an enrollment yet', async () => {
-      const token = await generateValidToken();
-
-      const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
-
-      expect(response.status).toEqual(httpStatus.NOT_FOUND);
-    });
-  });
 });
