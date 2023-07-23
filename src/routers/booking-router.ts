fc8b09d296d5fef69464433cc4b3b83c206fff2d
Anelizi <anelizicarvalho@gmail.com>
2023-07-23 18:58:38 -0300

    fix: PUT /booking/:bookingId

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { getBooking, postBooking } from '@/controllers/booking-controller';
+import { getBooking, postBooking, putBooking } from '@/controllers/booking-controller';
 
 const bookingRouter = Router();
 
-bookingRouter.all('/*', authenticateToken).get('/', getBooking).post('/', postBooking).put('/:bookingId');
+bookingRouter.all('/*', authenticateToken).get('/', getBooking).post('/', postBooking).put('/:bookingId', putBooking);
 
 export { bookingRouter };
